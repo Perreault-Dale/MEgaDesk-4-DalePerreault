b7488bcecca56fbf22233f8800abc31057ccbdc7
Dale Perreault <per15038@byui.edu>
2017-10-08 00:04:13 -0500

    Final commit

--- MegaDesk-3-DalePerreault/DisplayQuote.Designer.cs
+++ MegaDesk-3-DalePerreault/DisplayQuote.Designer.cs
@@ -28,34 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.viewOrdersGridView = new System.Windows.Forms.DataGridView();
+            this.custNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deskWidth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deskDepth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deskDrawers = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deskMaterials = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deskPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.viewOrdersGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // viewOrdersGridView
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(79, 64);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Empty";
+            this.viewOrdersGridView.AllowUserToAddRows = false;
+            this.viewOrdersGridView.AllowUserToDeleteRows = false;
+            this.viewOrdersGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.viewOrdersGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.custNameColumn,
+            this.deskWidth,
+            this.deskDepth,
+            this.deskDrawers,
+            this.deskMaterials,
+            this.orderDate,
+            this.deskPrice});
+            this.viewOrdersGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.viewOrdersGridView.Location = new System.Drawing.Point(0, 0);
+            this.viewOrdersGridView.Name = "viewOrdersGridView";
+            this.viewOrdersGridView.ReadOnly = true;
+            this.viewOrdersGridView.RowTemplate.Height = 28;
+            this.viewOrdersGridView.Size = new System.Drawing.Size(747, 529);
+            this.viewOrdersGridView.TabIndex = 0;
+            // 
+            // custNameColumn
+            // 
+            this.custNameColumn.HeaderText = "Name";
+            this.custNameColumn.Name = "custNameColumn";
+            this.custNameColumn.ReadOnly = true;
+            // 
+            // deskWidth
+            // 
+            this.deskWidth.HeaderText = "Width";
+            this.deskWidth.Name = "deskWidth";
+            this.deskWidth.ReadOnly = true;
+            // 
+            // deskDepth
+            // 
+            this.deskDepth.HeaderText = "Depth";
+            this.deskDepth.Name = "deskDepth";
+            this.deskDepth.ReadOnly = true;
+            // 
+            // deskDrawers
+            // 
+            this.deskDrawers.HeaderText = "Drawers";
+            this.deskDrawers.Name = "deskDrawers";
+            this.deskDrawers.ReadOnly = true;
+            // 
+            // deskMaterials
+            // 
+            this.deskMaterials.HeaderText = "Surface";
+            this.deskMaterials.Name = "deskMaterials";
+            this.deskMaterials.ReadOnly = true;
+            // 
+            // orderDate
+            // 
+            this.orderDate.HeaderText = "Order Date";
+            this.orderDate.Name = "orderDate";
+            this.orderDate.ReadOnly = true;
+            // 
+            // deskPrice
+            // 
+            this.deskPrice.HeaderText = "Price";
+            this.deskPrice.Name = "deskPrice";
+            this.deskPrice.ReadOnly = true;
             // 
             // DisplayQuote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 529);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(747, 529);
+            this.Controls.Add(this.viewOrdersGridView);
             this.Name = "DisplayQuote";
             this.Text = "DisplayQuote";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DisPlayQuote_closing);
+            ((System.ComponentModel.ISupportInitialize)(this.viewOrdersGridView)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView viewOrdersGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn custNameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deskWidth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deskDepth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deskDrawers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deskMaterials;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orderDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deskPrice;
     }
 }
--- MegaDesk-3-DalePerreault/DisplayQuote.cs
+++ MegaDesk-3-DalePerreault/DisplayQuote.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Windows.Forms;
 
@@ -9,25 +13,59 @@ namespace MegaDesk_4_DalePerreault
         public DisplayQuote()
         {
             InitializeComponent();
-            this.label1.Text = LabelText();
-        }
-
-        public string LabelText()
-        {
-            string rushCost = File.ReadAllText(@"rushOrderPrices.txt");
-            string[] rushSplit = rushCost.Split('\n');
-            int[] rushInts = new int[rushSplit.Length];
-            for (int i = 0; i < rushSplit.Length; i++)
-            {
-                rushInts[i] = int.Parse(rushSplit[i]);
-            }
-            var rushTest = rushInts[0] + ", " + rushInts[1] + ", " + rushInts[2];
-            return rushTest;
+            List<Deskquote> list = GetQuotes();
+            DataTable dt = CreateTable(list);
+            viewOrdersGridView.Columns.Clear();
+            viewOrdersGridView.DataSource = dt;
         }
 
         private void DisPlayQuote_closing(object sender, FormClosingEventArgs e)
         {
             FormProvider.menu1.Show();
+        }
+
+        private static DataTable CreateTable(List<Deskquote> quotes)
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("custName");
+            dt.Columns.Add("width");
+            dt.Columns.Add("depth");
+            dt.Columns.Add("drawers");
+            dt.Columns.Add("surface");
+            dt.Columns.Add("orderDate");
+            dt.Columns.Add("price");
+
+            foreach (var quote in quotes)
+            {
+                var row = dt.NewRow();
+                row["custName"] = quote.custName;
+                row["width"] = quote.custDesk.width;
+                row["depth"] = quote.custDesk.depth;
+                row["drawers"] = quote.custDesk.drawers;
+                row["surface"] = quote.custDesk.surface;
+                row["orderDate"] = quote.orderDate;
+                row["price"] = quote.price;
+
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        }
+
+        private List<Deskquote> GetQuotes()
+        {
+            StreamReader sr = new StreamReader("C:\\Users\\Dale\\source\\repos\\MegaDesk-4-DalePerreault\\MegaDesk-3-DalePerreault\\bin\\Debug\\quotes.json");
+            List<Deskquote> list = new List<Deskquote>();
+            while (!sr.EndOfStream)
+            {
+                string json = sr.ReadLine();
+                Deskquote j1 = JsonConvert.DeserializeObject<Deskquote>(json);
+                // MessageBox.Show(j1.custName + " " + j1.orderDate + " " + j1.price);
+                list.Add(j1);
+            }
+            sr.Close();
+            return list;
         }
     }
 }
